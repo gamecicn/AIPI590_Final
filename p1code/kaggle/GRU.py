aa2c9960a6686020c05e552c60a645e129a31ce2
Martin Guo <gbforever@163.com>
2022-04-07 15:39:01 -0400

    Finish train and eval for kaggle

--- p1code/kaggle/GRU.py
+++ p1code/kaggle/GRU.py
@@ -1,51 +1,103 @@
 import numpy as np
 import pandas as pd
-import os
-import argparse
 import time
-from utility import pad_history, calculate_hit
 import torch.nn as nn
 import torch as th
+from numpy.random import choice
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Run supervised GRU.")
 
-    parser.add_argument('--epoch', type=int, default=30,
-                        help='Number of max epochs.')
-    parser.add_argument('--epoch_size', type=int, default=-1,
-                        help='Each epoch include how many samples')
-    parser.add_argument('--data', nargs='?', default='data',
-                        help='data directory')
-    # parser.add_argument('--pretrain', type=int, default=1,
-    #                     help='flag for pretrain. 1: initialize from pretrain; 0: randomly initialize; -1: save the model to pretrain file')
-    parser.add_argument('--batch_size', type=int, default=256,
-                        help='Batch size.')
-    parser.add_argument('--hidden_factor', type=int, default=64,
-                        help='Number of hidden factors, i.e., embedding size.')
-    parser.add_argument('--gru_layer', type=int, default=1,
-                        help='GRU layer number.')
-    parser.add_argument('--r_click', type=float, default=0.2,
-                        help='reward for the click behavior.')
-    parser.add_argument('--r_buy', type=float, default=1.0,
-                        help='reward for the purchase behavior.')
-    parser.add_argument('--lr', type=float, default=0.01,
-                        help='Learning rate.')
-    return parser.parse_args()
+#================================================
+# Config
+#================================================
+
+args = {
+
+    # Dataset
+    "data"              : 'D:/workspace/Kaggle/HM_Recommend/Data/trans_500.csv',
+
+    "item_size"         : 104547,  # Fixed for HM  data set
+    "pad"               : 0,
+
+    "train_share"       : 0.8,  # training data share from whole data
+
+    # Model
+    "gru_layer"         : 1,
+    "hidden_factor"     : 64,
+    "state_len"         : 8,
+
+    # Train
+    "epoch"             : 30,
+    "epoch_size"        : -1,
+    "batch_size"        : 256,
+    "lr"                : 0.01,
+
+    # Log
+    "log_loss_period"   : 10,
+    "evaluate_period"   : 100,
+
+    # Evaluate
+
+    # Reward
+    "r_click"           : 1.0,
+    "r_buy"             : 1.0, # There is no buy in HM data set
+}
+
+DEVICE = 'cuda' if th.cuda.is_available() else 'cpu'
+
+#================================================
+# Utility
+#================================================
+
+def print_timing_msg(start_time, cur_cid, total_cid, cur_epoch, total_epoch):
+
+    cur_time = time.perf_counter()
+    elapse = (cur_time - start_time) / 60
+
+    all_epoch_cid = total_cid * total_epoch
+    finished_cid  = total_cid * cur_epoch + cur_cid
+
+    left = (all_epoch_cid / finished_cid) * elapse
+
+    print("Elapse %.2f mins, estimate %.2f mins left " % (elapse, left))
+
+
+
+def get_train_msg(args, num_batches, total_step):
+    msg = "==================================\n"  \
+        + "Modle: Basic GRU on ReplayBuffer\n"  \
+        + "GRU layer %d\n"      % (1) \
+        + "Embedding size %d\n" % (args.hidden_factor) \
+        + "Learn rate is %f\n"  % (args.lr) \
+        + "Total epoch %d\n"    % (args.epoch) \
+        + "Each epoch has %d batches, total %d batches\n" % (num_batches, total_step) \
+        + "=================================="
+    return msg
+
+
+
+#================================================
+# Model
+#================================================
 
 
 class GRUnetwork(nn.Module):
 
-    def __init__(self, item_num, embedding_size, num_layers = 1):
+    def __init__(self):
         super(GRUnetwork, self).__init__()
 
-        self.embedding = nn.Embedding(item_num+1,
+        embedding_size = args["hidden_factor"]
+
+        self.embedding = nn.Embedding(args["item_size"],
                                       embedding_size,
-                                      padding_idx = item_num,
+                                      padding_idx = args["pad"],
                                       max_norm=True)
 
-        self.gru = nn.GRU(embedding_size, embedding_size, batch_first = True, num_layers = num_layers)
+        self.gru = nn.GRU(embedding_size,
+                          embedding_size,
+                          batch_first = True,
+                          num_layers = args["gru_layer"])
 
-        self.fc = nn.Linear(embedding_size, item_num)
+        self.fc = nn.Linear(embedding_size, args["item_size"])
 
     def forward(self, input):
         x = self.embedding(input)
@@ -56,193 +108,197 @@ class GRUnetwork(nn.Module):
     def initHidden(self):
          pass
 
-def check_device():
-  return 'cuda' if th.cuda.is_available() else 'cpu'
+    @staticmethod
+    def create_model():
 
-def get_training_data(data_directory, batch_size):
+        model = GRUnetwork().to(DEVICE)
 
-    replay_buffer = pd.read_pickle(os.path.join(data_directory, 'replay_buffer.df'))
+        loss_fn = nn.CrossEntropyLoss(reduction="mean")
+        optimizer = th.optim.Adam(model.parameters(), lr=args['lr'])
 
-    data_statis = pd.read_pickle(
-        os.path.join(data_directory, 'data_statis.df'))  # read data statistics, includeing state_size and item_num
-    state_size = data_statis['state_size'][0]  # the length of history to define the state
-    item_num = data_statis['item_num'][0]  # total number of items
-
-    num_batches = int(replay_buffer.shape[0] / args.batch_size)
-
-    return replay_buffer, num_batches, item_num, state_size
+        return model, loss_fn, optimizer
 
 
-def create_model(item_num, args):
+def get_data():
 
-    model = GRUnetwork(item_num, args.hidden_factor, num_layers=args.gru_layer)
-    loss_fn = nn.CrossEntropyLoss(reduction="mean")
-    optimizer = th.optim.Adam(model.parameters(), lr=args.lr)
+    df = pd.read_csv(args["data"])
 
-    return model, loss_fn, optimizer
+    cus_id  = df['customer_id'].unique()
+    item_id = df['article_id'].unique()
+
+    code_to_item = {value: index for index, value in enumerate(item_id)}
+
+    df['article_id'] = df['article_id'].apply(lambda x: code_to_item[x])
+
+    train_cid = choice(cus_id, int(args["train_share"] * len(cus_id)), replace=False)
+
+    data_train = df[df["customer_id"].isin(train_cid)]
+    data_val = df[~df["customer_id"].isin(train_cid)]
+
+    return data_train, data_val, item_id
 
 
-def get_batch_data_from_replay_buffer(replay_buffer, batch_size, device):
+class ReplayBuffer():
 
-    batch = replay_buffer.sample(n=batch_size).to_dict()
-    rc_state = th.tensor(np.array(list(batch['state'].values()), dtype=np.int32)).to(device)
-    rc_action = th.tensor(np.array(list(batch['action'].values()), dtype=np.int64)).to(device)
+    def __init__(self, raw_data : pd.DataFrame, batch_size):
 
+        self.raw_data = raw_data
+
+        self.cid = raw_data['customer_id'].unique()
+        self.groups = self.raw_data.groupby(['customer_id'])
+
+        self.batch_size = batch_size
+        self.buffer_size = 16 * self.batch_size
+
+        self.buffer = []
+        self.reset()
+
+    def __iter__(self):
+        return self
+
+    def reset(self):
+        self.idx = 0
+        self.buffer.clear()
+
+    def progress(self):
+        return self.idx
+
+    def total(self):
+        return len(self.cid)
+
+    def pad_history(self, itemlist, length, pad_item):
+        if len(itemlist) >= length:
+            return itemlist[-length:]
+        if len(itemlist) < length:
+            temp = [pad_item] * (length - len(itemlist))
+            itemlist.extend(temp)
+            return itemlist
+
+    def conver(self, cid):
+        '''
+        Conver data for a certain cid into
+        :param cid:
+        :return:
+        '''
+        records = []
+        history = []
+
+        for index, row in self.groups.get_group(cid).iterrows():
+            s = list(history)
+            s = self.pad_history(s, args["state_len"], args["pad"])
+            action = row['article_id']
+            history.append(row['article_id'])
+            records.append((s, action, row['price'], row['sales_channel_id']))
+
+        return records
+    
+    def refill(self):
+        while self.idx < len(self.cid) and len(self.buffer) < self.buffer_size:
+            self.buffer.extend(self.conver(self.cid[self.idx]))
+            self.idx += 1
+
+    def __next__(self):
+
+        if len(self.buffer) < self.batch_size:
+            self.refill()
+
+        # if buffer size still smaller than a batch size after filling,
+        # it means all data was
+        if len(self.buffer) < self.batch_size:
+            raise StopIteration
+            
+        ret = self.buffer[:self.batch_size]
+        self.buffer = self.buffer[self.batch_size :]
+            
+        return ret
+
+
+def get_state_and_action(batch, device):
+    rc_state  = th.tensor([x[0] for x in batch], dtype=th.int32).to(device)
+    rc_action = th.tensor([x[1] for x in batch], dtype=th.int64).to(device)
     return rc_state, rc_action
 
 
-def print_training_msg(loss, step_cnt, total_step, start_time):
-    cur_time = time.perf_counter()
-    elapse = (cur_time - start_time) / 60
-    left_time = ((total_step - step_cnt) / step_cnt) * elapse
-
-    print("the loss in %dth batch is: %f" % (step_cnt, loss))
-    print("Elapse %f mins, estimate %f mins left " % (elapse, left_time))
-
-
-def get_train_msg(args, num_batches, total_step):
-
-    msg = "==================================\n"  \
-        + "Modle: Basic GRU on ReplayBuffer\n"  \
-        + "GRU layer %d\n"      % (1) \
-        + "Embedding size %d\n" % (args.hidden_factor) \
-        + "Learn rate is %f\n"  % (args.lr) \
-        + "Total epoch %d\n"    % (args.epoch) \
-        + "Each epoch has %d batches, total %d batches\n" % (num_batches, total_step) \
-        + "=================================="
-
-    return msg
-
-
-def evaluate(model, arg, state_size, item_num, eval_sample = 1000):
-
-    eval_data=pd.read_pickle(os.path.join(args.data, 'sampled_val.df'))
-    eval_ids = eval_data.session_id.unique()
-
-    groups = eval_data.groupby('session_id')
-    batch = 100
-    evaluated=0
-    total_clicks=0.0
-    total_purchase = 0.0
-    total_reward = [0, 0, 0, 0]
-    hit_clicks=[0,0,0,0]
-    ndcg_clicks=[0,0,0,0]
-    hit_purchase=[0,0,0,0]
-    ndcg_purchase=[0,0,0,0]
-    topk = [5, 10, 15, 20]
-    state_size = 10
+def evaluate(model, val_data):
 
     model.eval()
+    val_data.reset()
 
-    sample_ids = np.random.choice(eval_ids, eval_sample)
+    pred = np.array([], dtype=np.int32)
+    ans = np.array([], dtype=np.int32)
+    top12 = []
 
-    for id in sample_ids:
+    for batch in val_data:
 
-        states, len_states, actions, rewards = [], [], [], []
+        states, actions = get_state_and_action(batch, device=DEVICE)
 
-        group = groups.get_group(id)
+        logits = model(states).detach().numpy()
 
-        history=[]
+        predicts = np.argmax(logits, axis=1)
 
-        for index, row in group.iterrows():
-            state=list(history)
-            state=pad_history(state, state_size, item_num)
+        pred  = np.concatenate((pred, predicts), axis=0)
+        ans   = np.concatenate((ans,  actions), axis=0)
+        top12.extend([x[-12:] for x in np.argpartition(logits, (-12, -1))])
 
-            states.append(state)
-            action=row['item_id']
-            is_buy=row['is_buy']
+    acc1  = sum(pred == ans) / len(ans)
+    acc12 = sum([x in y for (x , y) in zip(ans, top12)]) / len(ans)
 
-            reward = arg.r_buy if is_buy == 1 else args.r_click
+    print(f"\n Eval ===========> Accuracy @1: {acc1} ,  @12:  {acc12}")
 
-            if is_buy==1:
-                total_purchase+=1.0
-            else:
-                total_clicks+=1.0
-
-            actions.append(action)
-            rewards.append(reward)
-            history.append(row['item_id'])
-
-        with th.no_grad():
-            prediction= model(th.Tensor(np.array(states)).type(th.int64))
-
-        sorted_list=np.argsort(prediction).reshape((-1, item_num))
-        calculate_hit(sorted_list,
-                      topk,
-                      actions,
-                      rewards,
-                      args.r_click,
-                      total_reward,
-                      hit_clicks,
-                      ndcg_clicks,
-                      hit_purchase,
-                      ndcg_purchase)
-
-    print('#############################################################')
-    print('total clicks: %d, total purchase:%d' % (total_clicks, total_purchase))
-    for i in range(len(topk)):
-        hr_click=hit_clicks[i]/total_clicks
-        hr_purchase=hit_purchase[i]/total_purchase
-        ng_click=ndcg_clicks[i]/total_clicks
-        ng_purchase=ndcg_purchase[i]/total_purchase
-        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        print('cumulative reward @ %d: %f' % (topk[i],total_reward[i]))
-        print('clicks hr ndcg @ %d : %f, %f' % (topk[i],hr_click,ng_click))
-        print('purchase hr and ndcg @%d : %f, %f' % (topk[i], hr_purchase, ng_purchase))
-    print('#############################################################')
+    return acc1, acc12
 
 
-def train_model(arg):
+def train_model():
 
-    device = check_device()
+    print("Training ==============================")
+    print(args)
+    print("\n\n\n")
 
     # get training data
-    replay_buffer, num_batches, item_num, state_size = get_training_data(args.data, args.batch_size)
+    data_train, data_val, item_id = get_data()
+
+    rb_train = ReplayBuffer(data_train, args["batch_size"])
+    rb_val   = ReplayBuffer(data_val, args["batch_size"])
 
     # get model, loss function and optimizer
-    model, loss_fn, optimizer = create_model(item_num, args)
+    model, loss_fn, optimizer = GRUnetwork.create_model()
 
-    total_step = num_batches * args.epoch
+    his_loss, his_eval1, his_eval2 = [], [], []
 
     start_time = time.perf_counter()
 
-    model.to(device)
+    for i in range(args['epoch']):
+        rb_train.reset()
 
-    step_cnt = 0
-
-    print(get_train_msg(args, num_batches, total_step))
-
-    for i in range(args.epoch):
-
-        print("\nStart of epoch %d" % (i,))
-
-        for j in range(num_batches):
+        for idx, batch in enumerate(rb_train):
 
             model.train()
-
             model.zero_grad()
 
-            states, actions = get_batch_data_from_replay_buffer(replay_buffer, args.batch_size, device)
+            states, actions = get_state_and_action(batch, device=DEVICE)
+
             logits = model(states)
-
             loss = loss_fn(logits, actions)
-            loss.backward()
 
+            his_loss.append(float(loss))
+
+            loss.backward()
             optimizer.step()
 
-            # Debug Msg
-            step_cnt += 1
+            # Message logging
+            if idx % args['log_loss_period'] == 0 and idx != 0:
+                print(f"Training: {rb_train.progress()}/{rb_train.total()} for  Epoch: {i}/{args['epoch']}: Loss : {loss}")
+                print_timing_msg(start_time, rb_train.progress(), rb_train.total(), i, args['epoch'])
 
-            if step_cnt % 50 == 0:
-                print_training_msg(loss, step_cnt, total_step, start_time)
-            if step_cnt % 50 == 0:
-                evaluate(model, arg, state_size, item_num)
+        # Evaluate after each epoch
+        eval = evaluate(model, rb_val)
+        his_eval1.append(eval[0])
+        his_eval2.append(eval[1])
 
+    return model, his_loss, (his_eval1, his_eval2)
 
 if __name__ == '__main__':
 
-    args = parse_args()
-
-    train_model(args)
+    model, his_loss, his_eval = train_model()
 
 
